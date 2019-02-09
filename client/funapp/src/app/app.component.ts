9586c005174c2f307b413e64408db4add655f866
mustafarslan <mustafarslan35@gmail.com>
2019-02-09 19:44:08 +0300

    Design has been updated with "Angular Material Design"

--- client/funapp/src/app/app.component.ts
+++ client/funapp/src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 
+export interface PeriodicElement {
+  groupNo: number;
+  receivedTime: string;
+  question1: boolean;
+  question2: boolean;
+  question3: boolean;
+  result: number
+}
+
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {groupNo: 1, receivedTime: new Date().toLocaleString(), question1: false, question2: false, question3: false, result: 0},
+  {groupNo: 2, receivedTime: new Date().toLocaleString(), question1: false, question2: false, question3: false, result: 0},
+  {groupNo: 3, receivedTime: new Date().toLocaleString(), question1: false, question2: false,  question3: false,result: 0},
+  {groupNo: 4, receivedTime: new Date().toLocaleString(), question1: false, question2: false,  question3: false, result: 0},
+  {groupNo: 5, receivedTime: new Date().toLocaleString(), question1: false, question2: false,  question3: false, result: 0},
+  {groupNo: 6, receivedTime: new Date().toLocaleString(), question1: false, question2: false,  question3: false, result: 0},
+  {groupNo: 7, receivedTime: new Date().toLocaleString(), question1: false, question2: false,  question3: false, result: 0},
+  {groupNo: 8, receivedTime: new Date().toLocaleString(), question1: false, question2: false,  question3: false, result: 0},
+  {groupNo: 9, receivedTime: new Date().toLocaleString(), question1: false, question2: false,  question3: false, result: 0},
+  {groupNo: 10, receivedTime: new Date().toLocaleString(), question1: false, question2: false,  question3: false, result: 0},
+  {groupNo: 11, receivedTime: new Date().toLocaleString(), question1: false, question2: false,   question3: false,result: 0},
+  {groupNo: 12, receivedTime: new Date().toLocaleString(), question1: false, question2: false,  question3: false, result: 0},
+  {groupNo: 13, receivedTime: new Date().toLocaleString(), question1: false, question2: false,  question3: false, result: 0},
+  {groupNo: 14, receivedTime: new Date().toLocaleString(), question1: false, question2: false,  question3: false, result: 0},
+  {groupNo: 15, receivedTime: new Date().toLocaleString(), question1: false, question2: false,  question3: false, result: 0},
+  {groupNo: 16, receivedTime: new Date().toLocaleString(), question1: false, question2: false,  question3: false, result: 0},
+  {groupNo: 17, receivedTime: new Date().toLocaleString(), question1: false, question2: false,  question3: false, result: 0},
+  {groupNo: 18, receivedTime: new Date().toLocaleString(), question1: false, question2: false,  question3: false, result: 0},
+  {groupNo: 19, receivedTime: new Date().toLocaleString(), question1: false, question2: false,  question3: false, result: 0},
+  {groupNo: 20, receivedTime: new Date().toLocaleString(), question1: false, question2: false,  question3: false, result: 0},
+
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'funapp';
+  title = 'Youth Code';
+  displayedColumns: string[] = ['groupNo', 'receivedTime', 'question1', 'question2', 'question3', 'result'];
+  dataSource = ELEMENT_DATA;
 }
--- client/funapp/src/app/app.module.ts
+++ client/funapp/src/app/app.module.ts
@@ -1,5 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatChipsModule, MatNativeDateModule,MatSnackBarModule,MatIconModule,MatDialogModule, MatButtonModule, MatTableModule, MatPaginatorModule , MatSortModule, MatCheckboxModule, MatToolbarModule, MatCard, MatCardModule, MatFormField, MatFormFieldModule, MatProgressSpinnerModule, MatInputModule } from  '@angular/material';
+import {MatDatepickerModule} from  '@angular/material/datepicker';
+import {MatRadioModule} from  '@angular/material/radio';
+import {MatSelectModule} from  '@angular/material/select';
+import {MatSliderModule} from  '@angular/material/slider';
+import {MatDividerModule} from  '@angular/material/divider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +18,54 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatChipsModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
+    MatDividerModule,
+    MatSliderModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatSortModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
+    MatInputModule,
+    MatPaginatorModule
+  ],
+  exports: [
+    MatChipsModule,
+    MatTabsModule,
+    MatDividerModule,
+    MatSliderModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatSortModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
+    MatInputModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
