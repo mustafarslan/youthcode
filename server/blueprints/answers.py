06654277cc93c639170f94c8c28c925f49a26c85
mustafarslan <mustafarslan35@gmail.com>
2019-02-10 14:16:28 +0300

    "POST" method has been updated at Server

--- server/blueprints/answers.py
+++ server/blueprints/answers.py
@@ -1,10 +1,34 @@
 from flask import Blueprint, request
+import json
+import os
+import sys
+current_dir = os.getcwd()
+uploads_dir = os.path.join(current_dir, 'uploads')
+sys.path.append(current_dir)
+sys.path.append(uploads_dir)
 
 answers = Blueprint('answers', __name__)
 
+
+def import_answer_class(name):
+    mod = __import__(name)
+    components = name.split('.')
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
+
+
 @answers.route('/api/answer', methods=('GET', 'POST'))
-def handleAnswers():
+def handle_answers():
     if request.method == "POST":
-        pass
+
+        file = request.files['file']
+        filename_no_extension = file.filename.split(".")
+        if file:
+            full_file_path = os.path.join(uploads_dir, file.filename)
+            file.save(full_file_path)
+            class_path = "uploads." + filename_no_extension[0]
+            module = import_answer_class(class_path)
+            return module.foo()
     else:
         return {"message":"Hello World"}
--- server/server.py
+++ server/server.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 app.register_blueprint(answers)
 app.response_class = CustomResponse
+app.config['UPLOAD_FOLDER'] = 'uploads/'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=False)
